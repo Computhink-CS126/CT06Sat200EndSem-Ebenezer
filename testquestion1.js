cb925d4a780d2f62b102af95c3d465b3a5419cc5
Computhink-CS126 <cs126@computhink.com.sg>
2025-10-25 07:18:57 +0000

    Oct 25, 2025, 7:18 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,16 @@
-
+let ballSize = 50;
+function setup(){
+    createCanvas(600, 400);
+    fill('limegreen');
+    posX = width/ 2;
+    xSpeed = 5;
+}
+function draw(){
+    background("lightblue")
+    // posX = constrain(posX, 25, width - 25)
+    circle(posX, 200, ballSize)
+    posX = posX - xSpeed
+    if (posX - ballSize/2 <= 0 || posX + ballSize/2 >= width) {
+        xSpeed = xSpeed * -1
+    }
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,16 @@
+let pokemons = ["Pikachu","Raichu","Bulbasaur","Mewtwo","Charizard"];
+let yPos;
 
+function setup(){
+    createCanvas(200, 300);
+    fill("pink");
+    textAlign(CENTER, CENTER);
+    textSize(20);
+    yPos = 100;
+    background(220);
+}
+function draw(){
+    text("Pikachu", 100, 50)
+    text("Raichu", 100, 100)
+    text
+}
